d74b042999271b84eb5beb62029463d72b207d3f
Cengiz Gürtusgil <cengiz.guertusgil@gmail.com>
2025-05-25 15:20:48 +0200

    make the build work

--- integration-tests/vite.config.js
+++ integration-tests/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,8 +12,9 @@ export default defineConfig({
   build: {
     manifest: true,
     outDir: '../target/classes/META-INF/resources',
+    emptyOutDir: true,
     rollupOptions: {
-      input: 'main.jsx'
+      input: resolve(__dirname, 'frontend/main.jsx')
     }
   }
 })
